8dcdf1dc3dbf73ad5c75ba4121fd029a59b727a7
alice <takemehighermore@gmail.com>
2016-08-07 23:15:26 +0900

    Created wordpress dockerfiles

---
+++ wordpress/create_db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# coding: utf-8
+
+if [[ $# -eq 0 ]]; then
+	echo "Usage: $0 <db_name>"
+	exit 1
+fi
+
+/usr/bin/mysqld_safe > /dev/null 2>&1 &
+
+echo "=> Creating database $1"
+RET=1
+while [[ RET -ne 0 ]]; do
+	sleep 5
+	mysql -uroot -e "CREATE DATABASE $1"
+	RET=$?
+done
+
+mysqladmin -uroot shutdown
+
+echo "=> Done!"
